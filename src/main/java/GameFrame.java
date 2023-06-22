39258ec549d9255648177008a778c26ff29da7d8
Admin <marciznrzeszowa@gmail.com>
2023-06-22 09:55:30 +0200

    Create

---
+++ src/main/java/GameFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Wąż");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
